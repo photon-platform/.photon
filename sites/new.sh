d154da0b90da35a2d086c9d18ce93c9e8d126ee2
i-am-phi <phi@i-am-phi.net>
2018-10-27 19:30:51 -0700

    new site creation update

--- sites/new.sh
+++ sites/new.sh
@@ -10,6 +10,7 @@ TITLE="$2"
 
 source ~/.photon/.hosts
 source ~/.photon/.sites
+source ~/.photon/.functions
 
 echo
 echo "✴ photon SITE Generator"
@@ -43,69 +44,77 @@ then
 
   START_TIME="$(date -u +%s)"
 
-  cd ~/SITES/starter
+  CLONE=starter
+
   sudo pwd
 
-  echo "✴ cloning starter"
-  bin/grav sandbox -s ../$PROJECT
+  echo
+  echo "✴ cloning grav"
+  cd ~/SITES/grav
+  bin/grav sandbox -s ~/SITES/$PROJECT
+  ln -sf ~/SITES/grav/.htaccess ~/SITES/$PROJECT/
 
-  echo "✴ symlink plugins"
-  cp -rf ~/SITES/starter/user/plugins/* ~/SITES/$PROJECT/user/plugins/
-  cp -rf ~/SITES/starter/user/blueprints/config/* ~/SITES/$PROJECT/user/blueprints/config/
-  cp -rf ~/SITES/starter/user/{accounts,blueprints,config} ~/SITES/$PROJECT/user/
-  cp -rf ~/SITES/starter/user/config/{fonts,media}.yaml ~/SITES/$PROJECT/user/config/
+  echo
+  echo "✴ clone starter user"
+  cd ~/SITES/$PROJECT
+  rm -rf user
+  git clone --recurse-submodules ~/SITES/$CLONE/user/.git ~/SITES/$PROJECT/user
+  git submodule foreach "pwd; \
+    git checkout master; \
+    git status -sb; \
+    echo"
 
-  cp -rr ~/SITES/starter/user/themes/photon-child ~/SITES/$PROJECT/user/themes/
-
-  cp -rf ~/SITES/starter/user/{.photon,README.md,CHANGELOG.md} ~/SITES/$PROJECT/user/
-  cp -rf ~/SITES/starter/user/{.gitignore,.sass-lint.yml} ~/SITES/$PROJECT/user/
 
   echo
   echo "✴ set config files"
-  cd ../$PROJECT/user
+  cd ~/SITES/$PROJECT/user
+
+  echo "✴ create server config folder"
+  mv starter.photon-platform.net $PROJECT.illumiphi.com
+
+  echo
+  echo "✴ set config files"
+  cd ~/SITES/$PROJECT/user
 
   # update theme
-  sed -i.bak "s/^\(\s*theme:\s*\).*/\1photon/" config/system.yaml
+  sed -i "s/^\(\s*theme:\s*\).*/\1photon/" config/system.yaml
   ag --nonumbers "theme:" config/system.yaml
 
   # update site title
-  sed -i.bak "s/^\(\s*title:\s*\).*/\1$TITLE/" config/site.yaml
+  sed -i "s/^\(\s*title:\s*\).*/\1$TITLE/" config/site.yaml
   ag --nonumbers "title:" config/site.yaml
 
   # update admin title
-  sed -i.bak "s/^\(\s*logo_text:\s*\).*/\1$TITLE/" config/plugins/admin.yaml
+  sed -i "s/^\(\s*logo_text:\s*\).*/\1$TITLE/" config/plugins/admin.yaml
   ag --nonumbers "title:" config/plugins/admin.yaml
 
   # update project key
-  sed -i.bak -e "s/^\(\s*export PROJECT=\).*/\1$PROJECT/" \
-             -e "s/ph.*net/illumiphi.com/g" \
-             .photon
+  sed -i -e "s/^\(\s*export PROJECT=\).*/\1$PROJECT/" \
+         -e "s/ph.*net/illumiphi.com/g" \
+         .photon
   ag --nonumbers "title:" .photon
 
-  echo
-  echo "✴ git"
-  git init
-  echo
-  echo "✴ add submodules"
+  # echo "✴ add submodules"
+  #
+  # # photon theme
+  # git submodule add https://github.com/photon-platform/grav-theme-photon.git themes/photon
+  #
+  # # plugins
+  # for PLUGIN in {photon,photon-{breadcrumbs,creation,event,form,organization,person,portfolio,project,search,subscribe}}; do
+  #   rm -rf plugins/$PLUGIN
+  #   echo
+  #   echo "✴ $PLUGIN"
+  #   git submodule add https://github.com/photon-platform/grav-plugin-$PLUGIN.git plugins/$PLUGIN
+  # done;
 
-  # photon theme
-  git submodule add git@github.com:photon-platform/grav-theme-photon.git themes/photon
-
-  # plugins
-  for PLUGIN in {photon,photon-{breadcrumbs,creation,event,form,organization,person,portfolio,project,search,subscribe}}; do
-    rm -rf plugins/$PLUGIN
-    echo
-    echo "✴ $PLUGIN"
-    git submodule add git@github.com:photon-platform/grav-plugin-$PLUGIN.git plugins/$PLUGIN
-  done;
-
-  git add .
-  git commit -m "init"
-  git remote add origin git@github.com:i-am-phi/$PROJECT.git
-  git push -fu origin master
 
   mkdir ~/SITES/LOGS/$PROJECT
   ~/.photon/sites/new-apache.sh $PROJECT
+
+  git add .
+  git commit -m "init"
+  git remote set-url origin git@github.com:i-am-phi/$PROJECT.git
+  git push -fu origin master
 
   END_TIME="$(date -u +%s)"
   ELAPSED="$(($END_TIME-$START_TIME))"
