63435bce6396d268f70a9a45bbd4f17abc9ca779
i-am-phi <phi@illumiphi.com>
2019-01-09 11:28:37 -0800

    grav

--- init/grav.sh
+++ init/grav.sh
@@ -3,8 +3,9 @@
 mkdir ~/SITES
 cd ~/SITES
 
-sudo wget https://getgrav.org/download/core/grav-admin/1.5.6 -O /var/www/grav-admin-1.5.6.zip
-sudo unzip grav-admin-1.5.6.zip grav/
+wget https://getgrav.org/download/core/grav-admin/1.5.6 -O /var/www/grav-admin-1.5.6.zip
+unzip grav-admin-1.5.6.zip
+mv grav-admin grav
 
 cp ~/.photon/templates/grav.conf /etc/apache2/sites-available/grav.conf
 
