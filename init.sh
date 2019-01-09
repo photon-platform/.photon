d49b722c5a5df5ee615968255eb19848c6cad27b
phi <phi@pop-os.localdomain>
2019-01-09 03:37:11 -0800

    init

--- init.sh
+++ init.sh
@@ -15,3 +15,13 @@ init/vbox-init.sh
 
 sudo apt install -y vim
 # **ctags** Combining vim with ctags yields a powerful combination for working with large or unfamiliar codebases.
+
+echo
+echo *****************************************
+echo
+atom -v
+apm list
+apache2 -v
+php -v
+git -v
+vim -v
