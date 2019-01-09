a9886676104b6fb37c7de97fb9f847d8be1e7f1a
i-am-phi <illumiphi@gmail.com>
2019-01-09 14:20:56 -0800

    grav

--- init/grav.sh
+++ init/grav.sh
@@ -1,15 +1,40 @@
-#!/bin/sh
+#!/bin/bash
+
+source ~/.photon/.hosts
 
 mkdir ~/SITES
+mkdir ~/SITES/LOGS
+mkdir ~/SITES/LOGS/grav
 cd ~/SITES
 
-wget https://getgrav.org/download/core/grav-admin/1.5.6 -O /var/www/grav-admin-1.5.6.zip
-unzip grav-admin-1.5.6.zip
-mv grav-admin grav
+# read -p "continue?" tmp
 
-cp ~/.photon/templates/grav.conf /etc/apache2/sites-available/grav.conf
+wget https://getgrav.org/download/core/grav-admin/1.5.6 -O grav-admin-1.5.6.zip
+unzip grav-admin-1.5.6.zip
+mv grav-admin/ grav/
+
+# git clone -b master https://github.com/getgrav/grav.git
+# cd grav
+# composer install --no-dev -o
+# bin/grav install
+
+# read -p "continue?" tmp
+
+
+sudo cp ~/.photon/templates/grav.conf /etc/apache2/sites-available/grav.conf
 
 sudo a2ensite grav.conf
-sudo systemctl restart apache2
 
-open grav.local
+addhost grav
+
+cd grav
+
+sudo chown -R phi:phi .
+find . -type f | sudo xargs chmod 664
+find ./bin -type f | sudo xargs chmod 775
+find . -type d | sudo xargs chmod 775
+find . -type d | sudo xargs chmod +s
+
+sudo apache2ctl restart
+
+open http://grav.local
