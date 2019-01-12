a51f7b4b716639caea75d27b7c8fd25fe54d8c42
i-am-phi <phi@illumiphi.com>
2019-01-11 17:43:44 -0800

    set grav permissions

--- init/grav.sh
+++ init/grav.sh
@@ -13,14 +13,6 @@ wget https://getgrav.org/download/core/grav-admin/1.5.6 -O grav-admin-1.5.6.zip
 unzip grav-admin-1.5.6.zip
 mv grav-admin/ grav/
 
-# git clone -b master https://github.com/getgrav/grav.git
-# cd grav
-# composer install --no-dev -o
-# bin/grav install
-
-# read -p "continue?" tmp
-
-
 sudo cp ~/.photon/templates/grav.conf /etc/apache2/sites-available/grav.conf
 
 sudo a2ensite grav.conf
@@ -29,7 +21,7 @@ addhost grav
 
 cd grav
 
-sudo chown phi:phi -R .
+sudo chown ${USERNAME}:${USERNAME} -R .
 find . -type f | sudo xargs chmod 664
 find ./bin -type f | sudo xargs chmod 775
 find . -type d | sudo xargs chmod 775
