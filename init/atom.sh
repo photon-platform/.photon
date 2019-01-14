6a739b055c4a36cc1233889974b1f4532ae4fb2e
i-am-phi <illumiphi@gmail.com>
2019-01-13 20:30:53 -0800

    atom - removed dependencies from install - removed beautify from packages

--- init/atom.sh
+++ init/atom.sh
@@ -3,9 +3,6 @@
 cd ~
 ln -sf ~/.photon/.atom
 
-# install atom dependencies
-sudo apt install -y gconf-service gconf-service-backend gconf2 gconf2-common libgconf-2-4
-
 # install atom
 sudo apt install -y atom
 
