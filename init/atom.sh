43fede85b15bf6c4a16430619800a76682b5c1bb
phi <phi@pop-os.localdomain>
2019-01-16 12:07:42 -0800

    don't install package sync separately

--- init/atom.sh
+++ init/atom.sh
@@ -6,7 +6,7 @@ ln -sf ~/.photon/.atom
 # install atom
 sudo apt install -y atom
 
-apm install package-sync
+# apm install package-sync
 
 # install each item from the packages file
 head -n -1 ~/.photon/.atom/packages.cson | sed -n "1d;p" | xargs apm install
