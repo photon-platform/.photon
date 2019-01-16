13d48863d6bcd32f1a7e4a397cfb2136bc81ef31
i-am-phi <phi@illumiphi.com>
2019-01-16 15:47:47 -0800

    removed terminator

--- init/thunderbird.sh
+++ init/thunderbird.sh
@@ -2,15 +2,4 @@
 
 
 # Thunderbird
-## to install most recent version of Mozilla Thunderbird, you can use the PPA maintained by the Mozilla team.
-## Next, update the system software packages using update command.
-## Once you’ve updated the system, install it using the following command.
-
-    # sudo add-apt-repository ppa:ubuntu-mozilla-security/ppa
-    # sudo apt-get update
-    sudo apt install -y thunderbird
-
-    # install lightening extension for calendar
-    sudo apt install -y terminator
-
-# install Chrome (no easy commandline)
+sudo apt install -y thunderbird
