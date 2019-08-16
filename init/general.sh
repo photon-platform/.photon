f7826a10216f99c77321ab9d06857be61a780583
i-am-phi <phi@photon-platform.net>
2019-08-15 17:35:06 -0700

    added ffmpeg

--- init/general.sh
+++ init/general.sh
@@ -15,6 +15,8 @@ sudo apt install -y ranger
 
 sudo apt install -y nnn
 
+sudo apt install -y ffmpeg
+
 sudo apt -y install python3-pip python3-setuptools python3-wheel
 
 wget -O ~/Downloads/rapid-photo-downloader.py https://launchpad.net/rapid/pyqt/0.9.14/+download/install.py
