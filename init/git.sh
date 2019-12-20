43b288e3579138594c87f49d307008eeffa78cfc
i-am-phi <phi@i-am-phi.net>
2019-12-20 06:52:16 -0800

    extended git credential cache for HTTPS

--- init/git.sh
+++ init/git.sh
@@ -5,4 +5,11 @@
 git config --global user.name "i-am-phi"
 git config --global user.email "illumiphi@gmail.com"
 
+
+git config --global credential.helper cache
+# Set git to use the credential memory cache
+git config --global credential.helper 'cache --timeout=3600'
+# Set the cache to timeout after 1 hour (setting is in seconds)
+
 git config --list
+
