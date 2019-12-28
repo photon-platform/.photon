e27aeb642de5dd3d9e8baf8bf0370320e4fb8796
i-am-phi <phi@i-am-phi.net>
2019-12-28 09:59:47 -0800

    adding go and pup

---
+++ init/go.sh
@@ -0,0 +1,6 @@
+cd ~/Downloads
+curl -O https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
+tar -xvf go1.13.5.linux-amd64.tar.gz
+sudo chown -R root:root ./go
+sudo mv go /usr/local
+
---
+++ init/pup.sh
@@ -0,0 +1,5 @@
+cd ~/bin
+curl -Lo pup.zip \
+  https://github.com/EricChiang/pup/releases/download/v0.3.7/pup_linux_amd64.zip 
+unzip pup.zip
+
