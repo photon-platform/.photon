0b645ead34d7b042c762cc2a4a5cabdaf8831b73
i-am-phi <phi@i-am-phi.net>
2020-08-18 09:34:11 -0700

    add neomutt

---
+++ init/neomutt.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo apt install -y isync
+sudo apt install -y pass
+sudo apt install -y neomutt
+
+git clone https://github.com/LukeSmithxyz/mutt-wizard
+cd mutt-wizard
+sudo make install
+
+
