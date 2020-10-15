c9a2c38d20cfda2c51b443261b8d49db2fe0517a
i-am-phi <phi@i-am-phi.net>
2020-10-15 09:20:30 -0700

    set branch on submodule update

--- tools/images.sh
+++ tools/images.sh
@@ -26,3 +26,39 @@ function images_ico() {
   fi
 }
 alias iico=images_ico
+
+function images_base64(){
+  # https://gist.github.com/puppybits/1565441/378180ab1d9b73a39099a24a2167318a426ee4bb
+  if [[ $1 ]]; then
+    # Remove escapes from the spaces
+    FILE_PATH=$(echo $1 | tr -s " " "\\ ")
+
+    filename=$(basename $FILE_PATH)
+    ext=${filename##*.}
+    if [ $ext == gif ]; then
+      append="data:image/gif;base64,";
+    elif [ $ext == jpeg ] || [ $ext == jpg ]; then
+      append="data:image/jpeg;base64,";
+    elif [ $ext == png ]; then
+      append="data:image/png;base64,";
+      echo "this worked {$FILE_PATH}"
+    elif [ $ext == svg ]; then
+      append="data:image/svg+xml;base64,";
+    elif [ $ext == ico ]; then
+      append="data:image/vnd.microsoft.icon;base64,";
+    fi
+
+    data=$( base64 < "$FILE_PATH" | tr -d '\n')
+
+    if [ "$#" -eq 2 ] && [ $2 == -img ]; then
+      data=\<img\ src\=\"$append$data\"\>
+    else
+      data=$append$data
+    fi
+    echo $data 
+  else
+    echo "Specify file to convert"
+  fi
+}
+alias ib64=images_base64
+
