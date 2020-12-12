c7ecaa5c4a56163cef678a843dc33a0a8e65c2b8
phiarchitect <phi@phiarchitect.com>
2020-12-12 10:44:34 -0800

    move host functions to tools

---
+++ tools/hosts/_main.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# copy from https://gist.github.com/irazasyed/a7b0a079e7727a4315b9
+
+# PATH TO YOUR HOSTS FILE
+export HOSTS_FILE=/etc/hosts
+
+# DEFAULT IP FOR HOSTNAME
+export IP="127.0.0.1"
+
+alias sites-conf="cd /etc/apache2/sites-available"
+alias hosts="sudo vim /etc/hosts"
+alias restart="sudo apache2ctl restart"
+
+removehost() {
+  if [ $1 ]
+  then
+    HOSTNAME="$1.local"
+    echo "removing host: $HOSTNAME";
+    if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
+    then
+        echo "$HOSTNAME Found in your $HOSTS_FILE, Removing now...";
+        sudo sed -i".bak" "/$HOSTNAME/d" $HOSTS_FILE
+        cat $HOSTS_FILE
+    else
+        echo "$HOSTNAME was not found in your $HOSTS_FILE";
+    fi
+  else
+    echo "specify project name: removehost my-web"
+  fi
+
+}
+
+addhost() {
+  if [ $1 ]
+  then
+    HOSTNAME="$1.local"
+    HOSTS_LINE="$IP\t$HOSTNAME"
+    echo "checking host: $HOSTNAME";
+    if [ -n "$(grep $HOSTNAME $HOSTS_FILE)" ]
+        then
+            echo "$HOSTNAME already exists : $(grep $HOSTNAME $HOSTS_FILE)"
+        else
+            echo "Adding $HOSTNAME to your $HOSTS_FILE";
+            sudo -- sh -c -e "echo '$HOSTS_LINE' >> $HOSTS_FILE";
+
+            if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
+                then
+                    echo "$HOSTNAME was added succesfully \n $(grep $HOSTNAME $HOSTS_FILE)";
+                else
+                    echo "Failed to Add $HOSTNAME, Try again!";
+            fi
+    fi
+    cat $HOSTS_FILE
+  else
+    echo "specify project name: addhost my-web"
+  fi
+
+}
