ca27b213efb70e6b7f8b16d1f6119ab76f629a03
phiarchitect <phi@phiarchitect.com>
2020-12-14 12:07:25 -0800

    change dir for grav update

--- tools/grav/_main.sh
+++ tools/grav/_main.sh
@@ -4,6 +4,10 @@ source ~/.photon/tools/grav/actions.sh
 
 alias V=tools_grav
 
+# alias grav-core="wget -O _grav-core.zip https://getgrav.org/download/core/grav/1.6.9 "
+# alias grav-admin="wget -O _grav-admin.zip https://getgrav.org/download/core/grav/1.6.9 "
+# alias grav-update="cd $SITESROOT/grav;bin/gpm self-upgrade;"
+
 function tools_grav() {
   @
   source .photon
--- tools/grav/actions.sh
+++ tools/grav/actions.sh
@@ -7,7 +7,12 @@ function tools_grav_actions() {
   read -s -n1 -p "$P > " action
   case $action in
     q) clear; ;; # quit
-    v) bin/gpm self-upgrade; echo; tools_grav_actions; ;;
+    v) 
+        cd $SITESROOT/grav
+        bin/gpm self-upgrade; 
+        cd -
+        echo; tools_grav_actions; 
+        ;;
     u) bin/gpm update; bin/grav clean; echo; tools_grav_actions; ;;
     c) bin/grav clearcache; echo; tools_grav_actions; ;;
     l) bin/grav clean; echo; tools_grav_actions; ;;
