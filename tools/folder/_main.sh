7b96f59e74dc5bb5798adb22200ba77ef55333bf
phiarchitect <phi@phiarchitect.com>
2020-12-27 06:56:46 -0800

    improve file extension summarization

--- tools/folder/_main.sh
+++ tools/folder/_main.sh
@@ -17,13 +17,7 @@ function folder() {
   h2 "$(( siblings_index + 1 )) ${fgg08}of${txReset} $siblings_count"
   show_dir
 
-  ui_banner "files $SEP $(find "$PWD" -type f ! -wholename "*.git/*" | wc -l)"
-  echo
-  # find $PWD -type f ! -wholename "*.git/*" | xargs -I {} basename "{}" | sed 's/.*\.\(.*\)$/\1/' | sort | uniq -c | sort -nr | head -n20
-  # list_working_files | xargs -I {} basename "{}" | sed 's/.*\.\(.*\)$/\1/' | sort | uniq -c | sort -nr | head -n20
-  list_working_files | xargs -I {} basename "{}" |  sed 's/.*\.\(.*\)$/\1/' | sort | uniq -c | sort -nr | head -n20
-
-  echo
+  folder_summarize
 
   folder_children
   h1 "Total $SEP $(folder_total_bytes "$PWD") ${fgg08}KiB${txReset}"
@@ -33,5 +27,26 @@ function folder() {
 }
 
 function folder_total_bytes() {
- printf "%'.f\n" $( du -s "$1" | awk '{ print $1}' ) 
+  printf "%'.f\n" $( du -s "$1" | awk '{ print $1}' ) 
+}
+
+function folder_summarize() {
+  ui_banner "files $SEP $(find "$PWD" -type f ! -wholename "*.git/*" | wc -l)"
+  echo
+   
+  # list_working_files | xargs -I {} basename "{}" |  sed 's/.*\.\(.*\)$/\1/' | sort | uniq -c | sort -nr | head -n20
+
+  declare -A ext_summary
+  extensions=$( list_working_files | xargs -I {} basename "{}" |  sed 's/.*\.\(.*\)$/\1/' )
+  for ext in ${extensions[@]}; do
+    (( ext_summary[$ext]++ ))
+  done
+
+  for key in ${!ext_summary[@]}
+  do
+    printf " %6d $SEP %s\n" "${ext_summary[$key]}" $key
+  done | sort -rn | head -n20
+
+  echo
+  
 }
