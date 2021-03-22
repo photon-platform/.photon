d561169a8c0b5a21cc668bc001ff624ed76d647e
phiarchitect <phi@phiarchitect.com>
2021-03-22 16:09:42 -0700

    add speak_file function

--- tools/speak.sh
+++ tools/speak.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 function speak() {
-  espeak -v mb-en1  -k44 "$@" 
+  espeak -v mb-en1 -s 150  "$*" 
 
 }
 
 function now() {
-  
   speak "$(date "+%A, %B %e, %I:%M %p")"
+}
+
+function speak_file() {
+  while read line 
+  do 
+      speak "$line" 
+  done < $1
 }
