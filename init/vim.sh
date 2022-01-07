1b46e4c87f86898cfc43b1caa769c23cb7f8f9aa
phiarchitect <phi@phiarchitect.com>
2022-01-07 11:56:16 -0800

    clean up init files + vim install

--- init/vim.sh
+++ init/vim.sh
@@ -5,24 +5,32 @@ source ~/.photon/ui/_main.sh
 clear -x
 ui_banner "vim"
 
-# echo
-# h1 "cmake"
-# echo
-# sudo apt install -y cmake #for YouCompleteMe
+echo
+h1 "build-essential cmake for YouCompleteMe"
+echo
+sudo apt install -y build-essential cmake 
 
 echo
 h1 "exuberant-ctags"
 echo
 sudo apt install -y exuberant-ctags
 
-# echo
-# h1 "vim"
-# echo
+echo
+h1 "vim"
+echo
 # sudo apt install -y vim
-# # enables + register for system clipboard
-# sudo apt install -y vim-gtk3
 
-echo
-h1 "install Vundle plugins"
-echo
-vim +PluginInstall +qall
+# # enables + register for system clipboard
+sudo apt install -y vim-gtk
+
+# echo
+# h1 "install Vundle plugins"
+# echo
+# vim +PluginInstall +qall
+git clone git@github.com:photon-platform/.vim --recurse-submodules
+~/.vim/set_vimrc.sh
+
+# build YouCompleteMe util
+cd ~/.vim/photon/completion
+git submodule update --init --recursive
+python3 install.py
