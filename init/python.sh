4e77b90f5ad62c2589d77f4470006a2e864ff391
phiarchitect <phi@phiarchitect.com>
2022-01-07 14:15:12 -0800

    set -y on pip install

--- init/python.sh
+++ init/python.sh
@@ -8,7 +8,7 @@ echo
 # echo
 # sudo apt install libgl1-mesa-glx libegl1-mesa libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 
 # anaconda=Anaconda3-2021.11-Linux-x86_64.sh
 # echo
