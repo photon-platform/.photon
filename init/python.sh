c123a442b4f7d75240710a336ad00deaf45eb7e7
phiarchitect <phi@phiarchitect.com>
2022-03-05 06:19:52 -0800

    add python3-gi-cairo

--- init/python.sh
+++ init/python.sh
@@ -10,7 +10,7 @@ echo
 
 sudo apt install -y python3-pip
 sudo apt install -y python3-venv
-
+sudo apt install -y python3-gi-cairo
 # h1 "Qt5"
 # echo
 # sudo apt install -y qtbase5-dev qttools5-dev qttools5-dev-tools qtwebengine5-dev
