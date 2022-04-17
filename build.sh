6aaa5934547c48e6722090b24b48f25dff43ef94
phiarchitect <phi@phiarchitect.com>
2022-04-17 11:53:20 -0700

    correct path

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ source ~/.bashrc
 
 timidity *.mid -Ov 
 
-ffmpeg -y -f concat -i sequence.txt -i dorian-144-$1.ogg -r 60 build.mp4
+ffmpeg -y -f concat -i sequence.txt -i sequence.ogg -r 60 sequence.mp4
 
-mpv build.mp4 --keep-open=yes
+mpv build.mp4 --keep-open=yes --fs
