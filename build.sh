4cb0cc8fd382b8375fccc55444260811871cf0cd
phiarchitect <phi@phiarchitect.com>
2022-04-17 12:07:35 -0700

    typo

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ cd $1
 
 source ~/.bashrc
 
-timidity *.mid -Ov 
+timidity sequence.mid -Ov 
 
 ffmpeg -y -f concat -i sequence.txt -i sequence.ogg -r 60 sequence.mp4
 
-mpv build.mp4 --keep-open=yes --fs
+mpv sequence.mp4 --keep-open=yes --fs
