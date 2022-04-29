b1f98559006c2304b7bd6d5dd5d41a924340ec59
phiarchitect <phi@phiarchitect.com>
2022-04-29 12:39:26 -0700

    update python install

--- init/python.sh
+++ init/python.sh
@@ -4,11 +4,14 @@ source ~/.photon/ui/_main.sh
 clear -x
 ui_banner "Python"
 echo
-# h1 "Anaconda Prerequisites"
-# echo
-# sudo apt install libgl1-mesa-glx libegl1-mesa libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 
-sudo apt install -y python3-pip
+sudo apt install -y python3
+sudo apt install -y python3-dev
+
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+# sudo apt install -y python3-pip
 sudo apt install -y python3-venv
 sudo apt install -y python3-gi-cairo
 # h1 "Qt5"
@@ -21,7 +24,7 @@ sudo apt install -y python3-gi-cairo
 # sudo apt install -y python3-dev python3-pip python3-lxml python3-six python3-css-parser python3-dulwich
 # sudo apt install -y python3-tk python3-pyqt5 python3-pyqtwebengine python3-html5lib python3-regex python3-pillow python3-cssselect python3-chardet
 
-pip install -U pip
+# pip install -U pip
 
 pip install -U jupyterlab
 pip install -U sympy
