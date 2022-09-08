246c898b33dd60a467a4fe7bdf0f0e5ed1af80da
phiarchitect <phi@phiarchitect.com>
2022-09-07 20:27:32 -0700

    initialize nvm before calling

--- init/node.sh
+++ init/node.sh
@@ -16,20 +16,21 @@ sub "nvm"
 echo
 # sudo apt install -y npm
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source ~/.bashrc
 nvm install --lts
 
 sub "timecut & timesnap"
 npm i -g timesnap timecut
 
-# sub "editly"
-# sudo apt install -y \
-  # build-essential \
-  # libcairo2-dev \
-  # libpango1.0-dev \
-  # libjpeg-dev \
-  # libgif-dev \
-  # librsvg2-dev
-# npm i -g canvas
-# npm i -g editly
+sub "editly"
+sudo apt install -y \
+  build-essential \
+  libcairo2-dev \
+  libpango1.0-dev \
+  libjpeg-dev \
+  libgif-dev \
+  librsvg2-dev
+npm i -g canvas
+npm i -g editly
 
 
