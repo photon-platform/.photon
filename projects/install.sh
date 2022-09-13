e3f53203f547645573075cf1c5b88ab44a7cc93f
phiarchitect <phi@phiarchitect.com>
2022-09-13 07:09:09 -0700

    complete projects install

--- projects/install.sh
+++ projects/install.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+mkdir -p ~/Projects/PHIARCHITECT
+cd ~/Projects/PHIARCHITECT
+
+git clone git@github.com:phiarchitect/phiarchitect
+git clone git@github.com:phiarchitect/phiarchitect.com
+git clone git@github.com:phiarchitect/phiarchitect.github.io
+
+
 mkdir -p ~/Projects/GEOMETOR
 cd ~/Projects/GEOMETOR
 
@@ -13,11 +21,21 @@ git clone git@github.com:geometor/phyllotaxis
 git clone git@github.com:geometor/polynumbers
 git clone git@github.com:geometor/relop
 
+
 mkdir -p ~/Projects/PHOTON
 cd ~/Projects/PHOTON
 
 git clone git@github.com:photon-platform/photon-sphinx-theme
+pip install -e photon-sphinx-theme
 
+git clone git@github.com:photon-platform/photon-platform.github.io
+
+
+mkdir -p ~/Projects/HARMONIC
+cd ~/Projects/HARMONIC
+
+git clone git@github.com:harmonic-resonance/phi-midi
+pip install -e phi-midi
 
 
 
