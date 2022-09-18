4e8f73b52d50dd3ba928010de31905dbb29e3f11
phiarchitect <phi@phiarchitect.com>
2022-09-18 08:12:35 -0700

    create project folder on get

--- tools/capture/actions.sh
+++ tools/capture/actions.sh
@@ -2,7 +2,6 @@
 capture_project=$USER
 
 function tools_capture_actions() {
-
   echo
   hr
   P=" ${fgYellow}CAPTURE${txReset}"
@@ -29,12 +28,13 @@ function tools_capture_actions() {
 function tools_capture_get_all_files() {
   capture_project=$( ask_value "project" "$capture_project" )
   capture_project=$( slugify "$capture_project" )
+  mkdir -p $capture_project
 
   capture_subject=$( ask_value "subject" "$capture_subject" )
   capture_subject=$( slugify "$capture_subject" )
   
   # gphoto2 --get-all-files --filename "${HOME}/Media/${capture_project}/%y.%j.%H%M%S.${capture_subject}-%03n.%C" 
-  gphoto2 --get-all-files --filename "%y.%j.%H%M%S.${capture_subject}-%03n.%C" 
+  gphoto2 --get-all-files --filename "${capture_project}/%y.%j.%H%M%S.${capture_subject}-%03n.%C" 
 }
 
 function tools_capture_delete_all_files() {
