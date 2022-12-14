acf2d916a00b4ccc514ea8fac732e5b9927763a2
phiarchitect <phi@phiarchitect.com>
2022-12-14 10:01:31 -0800

    setup site

---
+++ conf.py
@@ -0,0 +1,34 @@
+from ablog.conf import *
+
+org = 'photon-platform'
+org_name = 'PHOTON platform'
+
+repo = '.photon'
+repo_name = '.photon'
+
+blog_title = f'{org_name} • {repo_name}'
+project = f'{org_name}<br/>•<br/>{repo_name}'
+version = ""  # The short X.Y version.
+release = ""  # The full version, including alpha/beta/rc tags.
+
+copyright = f'{year}, {org_name}'
+author = "phi ARCHITECT"
+
+# Base URL for the website, required for generating feeds.
+# e.g. blog_baseurl = "http://example.com/"
+blog_baseurl = f'https://{org}.github.io/{repo}'
+html_base_url = blog_baseurl
+html_baseurl = blog_baseurl
+
+blog_authors = {
+    "phi": ("phi ARCHITECT", None),
+}
+
+html_theme_options = {
+    'logo': 'logo.png',
+    'logo_name': True,
+    'github_user': org,
+    'github_repo': repo,
+    'github_button': True,
+}
+
---
+++ index.rst
@@ -0,0 +1,37 @@
+
+PHOTON platform • logger
+========================
+.. include:: README.rst
+
+
+recent logs:
+------------
+
+.. postlist:: 5
+   :excerpts:
+
+
+urgent todos:
+-------------
+
+.. postlist:: 
+   :category: TODO
+   :tags: urgent
+   :excerpts:
+
+
+
+.. toctree::
+   :maxdepth: 2
+
+   modules/photonplatform.logger.rst
+
+
+
+
+.. Indices and tables
+.. ------------------
+
+.. * :ref:`genindex`
+.. * :ref:`modindex`
+.. * :ref:`search`
