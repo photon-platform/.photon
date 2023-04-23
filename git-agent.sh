011fc8ad7f31d2b8899797f84ba772fa9579dacc
phiarchitect <phi@phiarchitect.com>
2023-04-22 17:02:00 -0700

    setup and git agent

--- git-agent.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [ -z "$SSH_AUTH_SOCK" ]; then
-   # eval "$(ssh-agent -s)"
-   eval `ssh-agent`
-   ssh-add ~/.ssh/id_ed25519
-fi
-
--- setup_projects.py
+++ setup_projects.py
@@ -1,3 +1,41 @@
+"""
+Setup GitHub Repositories
+
+This script clones and installs a set of GitHub repositories for specified
+GitHub accounts into a local directory. The repository information is loaded
+from a separate YAML file called 'project_info.yaml'.
+
+The script creates a folder for each account under the '~/PROJECTS' directory,
+and clones each repository into its respective account folder. If a repository
+is marked as 'installed' in the 'project_info.yaml' file, the script will
+install it using `pip install -e .`.
+
+Usage:
+    python setup_github_repos.py
+
+Requirements:
+    - PyYAML
+
+The 'project_info.yaml' file should have the following format:
+
+```yaml
+account_name:
+  - name: repository_name
+    installed: true/false
+  - name: another_repository_name
+    installed: true/false
+```
+
+Example 'project_info.yaml' content:
+
+```yaml
+phiarchitect:
+  - name: phiarchitect
+    installed: false
+  - name: phiarchitect.com
+    installed: false
+```
+"""
 import os
 import subprocess
 import yaml
@@ -17,6 +55,7 @@ def setup_repos(account, repo_list):
 
     for repo in repo_list:
         repo_name = repo["name"]
+        print(f"\n\n{account} - {repo_name}\n\n")
         repo_url = f"git@github.com:{account}/{repo_name}"
         repo_path = os.path.join(account_dir, repo_name)
         if not os.path.exists(repo_path):
